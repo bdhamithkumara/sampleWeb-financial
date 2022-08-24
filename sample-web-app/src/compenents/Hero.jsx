09f231e5db2c27eab03bc671de755a22075a467b
Damithkumara <damithkumararoxx55@gmail.com>
2022-08-24 09:14:22 +0530

    complete hero section

--- sample-web-app/src/compenents/Hero.jsx
+++ sample-web-app/src/compenents/Hero.jsx
@@ -16,6 +16,7 @@ const Hero = () => {
                     backSpeed={140} loop />
             </div>
             <p className='md:text-2xl text-xl font-bold text-gray-500'>Monitor your data analytics to increase revenue for BTB, BTC, & SASS platforms</p>
+            <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black'>Get Started</button>
         </div>
     </div>
   )
